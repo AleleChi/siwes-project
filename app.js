8941a3765ea21cc41cc6972da2f6b8a624bff41d
AleleChi <alelechi17@gmail.com>
2024-03-07 22:42:04 +0100

    mobile view

---
+++ app.js
@@ -0,0 +1,8 @@
+function toggleMenu(){
+    var nav = document.getElementById("myNav");
+    if (nav.style.display === "block"){
+        nav.style.display ="none"
+    } else {
+        nav.style.display ="block"
+    }
+}
